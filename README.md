777b878d5b176158ba40ff5eb74ef8dc62830cff
Brad Smith <smithfamily42@gmail.com>
2021-05-30 23:24:25 -0700

    udpating main

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Weather API
 GitHub repo: https://github.com/smithfamily42/challenge6
 live URL: https://smithfamily42.github.io/challenge6/
 
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,8 @@ form.addEventListener("submit", event => {
         }
        
     }
+    let priorCity = localStorage.getItem("last location");
+    console.log(priorCity);
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=standard`;
 
@@ -55,7 +57,6 @@ form.addEventListener("submit", event => {
       let icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         weather[0]["icon"]
       }.svg`;
-console.log(data);
       let div = document.createElement("div");
       div.classList.add("mainContent");
       let markup = `
@@ -69,6 +70,7 @@ console.log(data);
       <p>Temp: ${Math.round((((main.temp) - 273.5) * 1.8) + 32)}<sup>°F</sup></p>
       <p>Humidity: ${main.humidity} %</p>
       <p>Wind: ${((wind.speed) * 2.237).toFixed(1)} MPH</p>
+      <p>UV:</p>
     `;
       div.innerHTML = markup;
       document.getElementById("city-info").appendChild(div);
@@ -83,3 +85,39 @@ console.log(data);
   input.focus();
 });
 
+function UVIndex(ln,lt){
+  //lets build the url for uvindex.
+  var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+"&lat="+lt+"&lon="+ln;
+  $.ajax({
+          url:uvqURL,
+          method:"GET"
+          }).then(function(response){
+              $(currentUvIndex).html(response.value);
+          });
+}
+/*
+function forecast(cityid){
+  var dayover= false;
+  var queryforecastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+apiKey;
+  $.ajax({
+      url:queryforecastURL,
+      method:"GET"
+  }).then(function(response){
+      
+      for (i=0;i<5;i++){
+          var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+          var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+          var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+          var tempK= response.list[((i+1)*8)-1].main.temp;
+          var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+          var humidity= response.list[((i+1)*8)-1].main.humidity;
+      
+          $("#fDate"+i).html(date);
+          $("#fIcon"+i).html("<img src="+iconurl+">");
+          $("#fTemp"+i).html(tempF+"&#8457");
+          $("#fHumidity"+i).html(humidity+"%");
+      }
+      
+  });
+}
+*/
